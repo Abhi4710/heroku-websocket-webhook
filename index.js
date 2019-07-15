e313c35f6634b071aaaa880e5780edbc8e609739
Abhi4710 <34485116+Abhi4710@users.noreply.github.com>
2019-07-15 19:55:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,11 @@
-'use strict';
+const WebSocket = require('ws');
 
-const res_dict = {"hello": "Hi", "name":"Abhi", "server":"websocket", "hi": "hello", "version": "1.0",  '{"state":"ON"}': {"query": "cmd", "state": "on"}, '{"state":"OFF"}': {"query": "cmd", "state": "off"}}
+const ws = new WebSocket('ws://demos.kaazing.com/echo');
 
-const express = require('express');
-const SocketServer = require('ws').Server;
-const path = require('path');
-
-const PORT = process.env.PORT || 3000;
-
-const server = express().listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-const wss = new SocketServer({ server });
-
-wss.on('connection', function connection(ws) {
-  console.log('Client connected');
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-    let res = res_dict[message]
-    // ws.send(res);
-    if (message == '{"state":"ON"}'){
-      res = res_dict['{"state":"ON"}']
-    }
-    if (message == '{"state":"OFF"}'){
-      res = res_dict['{"state":"OFF"}']
-    }
-    if (message != ''){
-    myfunction(res)
-    };
-  });
-  ws.on('close', () => console.log('Client disconnected'));
+ws.on('open', function open() {
+  ws.send('something');
 });
 
-function myfunction(res) {
-  wss.clients.forEach((client) => {
-    client.send(JSON.stringify(res));
-  });
-};
+ws.on('message', function incoming(data) {
+  console.log(data);
+});
