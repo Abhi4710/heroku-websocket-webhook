b83091a864a6eb4c3055803eb6bdb38e287cd9e7
Abhi4710 <34485116+Abhi4710@users.noreply.github.com>
2019-07-16 13:43:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ const INDEX = path.join(__dirname, 'index.html');
 
 const PORT = process.env.PORT || 3000;
 
-const server = express().use((req, res) => res.sendFile(INDEX) ).listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
+var server = express().use((req, res) => res.sendFile(INDEX) ).listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 server.use(
   bodyParser.urlencoded({
