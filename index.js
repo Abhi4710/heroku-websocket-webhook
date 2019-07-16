68b0e3890c1493531c76af143a67869902d6f73f
Abhi4710 <34485116+Abhi4710@users.noreply.github.com>
2019-07-17 00:46:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ server.ws('/', function(ws, req) {
 
 
 function myfunction(resp) {
-    wsInstance.getWss().clients.forEach((client) => {
+    expressWs.wsInstance.getWss().clients.forEach((client) => {
         client.send(JSON.stringify(qtext));
         // console.log(client.send('1234'));
     });
