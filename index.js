0d2a26d2e916a65539ecd9e0e1c2d131e86aa0b0
Abhi4710 <34485116+Abhi4710@users.noreply.github.com>
2019-07-16 17:55:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const express = require('express');
-const ws_server = express();
+const ws_server = express().use((req, res) => res.sendFile(INDEX)).listen(PORT, () => console.log(`Listening on ${PORT}`));
 const SocketServer = require('ws').Server;
 const path = require('path');
 
@@ -9,9 +9,7 @@ const INDEX = path.join(__dirname, 'index.html');
 
 const PORT = process.env.PORT || 3000;
 
-ws_server.use((req, res) => res.sendFile(INDEX)).listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-const wss = new SocketServer({ws_server})
+const wss = new SocketServer({ ws_server })
 
 wss.on('connection', function connection(ws) {
     console.log('Client connected');
