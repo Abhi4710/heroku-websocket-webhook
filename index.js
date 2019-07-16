a5e0b39d819eb5f03de8f031a1cd7ca27be08c77
Abhi4710 <34485116+Abhi4710@users.noreply.github.com>
2019-07-16 14:18:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,28 @@ const INDEX = path.join(__dirname, 'index.html');
 
 const PORT = process.env.PORT || 3000;
 
-var server = express().use((req, res) => res.sendFile(INDEX) );
+var server = express().use((req, res) => res.sendFile(INDEX) ).listen(PORT, () => console.log(`Listening on ${ PORT }`));;
+
+const wss = new SocketServer({server});
+console.log(wss);
+wss.on('connection', function connection(ws) {
+  console.log('Client connected');
+//   ws.on('message', function incoming(message) {
+//     console.log('received: %s', message);
+//     let res = res_dict[message]
+//     // ws.send(res);
+//     if (message == '{"state":"ON"}'){
+//       res = res_dict['{"state":"ON"}']
+//     }
+//     if (message == '{"state":"OFF"}'){
+//       res = res_dict['{"state":"OFF"}']
+//     }
+//     if (message != ''){
+//     myfunction(res)
+//     };
+//   });
+  ws.on('close', () => console.log('Client disconnected'));
+});
 // .use(
 //   bodyParser.urlencoded({
 //     extended: true
@@ -70,29 +91,9 @@ server.post("/echo", function(req, res) {
 });
 
 // server.use((req, res) => res.sendFile(INDEX) ).listen(PORT, () => console.log(`Listening on ${ PORT }`));
-server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+// server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 // const wss = new SocketServer({ server });
-const wss = new SocketServer({'ws://esp-gas.herokuapp.com'})
-console.log(wss);
-wss.on('connection', function connection(ws) {
-  console.log('Client connected');
-//   ws.on('message', function incoming(message) {
-//     console.log('received: %s', message);
-//     let res = res_dict[message]
-//     // ws.send(res);
-//     if (message == '{"state":"ON"}'){
-//       res = res_dict['{"state":"ON"}']
-//     }
-//     if (message == '{"state":"OFF"}'){
-//       res = res_dict['{"state":"OFF"}']
-//     }
-//     if (message != ''){
-//     myfunction(res)
-//     };
-//   });
-  ws.on('close', () => console.log('Client disconnected'));
-});
 
 function myfunction(res) {
   wss.clients.forEach((client) => {
