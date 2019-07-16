eff81d7f5f78f6a283c3e6fd9b227a6466936b33
Abhi4710 <34485116+Abhi4710@users.noreply.github.com>
2019-07-17 01:02:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ server.ws('/', function(ws, req) {
   console.log('socket', req.testing);
 });
 
-var wsInstance = server.wsInstance.app;
+var wsInstance = server.wsInstance.server;
 function myfunction(resp) {
     wsInstance.getWss().clients.forEach((client) => {
         client.send(JSON.stringify(qtext));
