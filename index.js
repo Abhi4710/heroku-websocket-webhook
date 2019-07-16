99cc934ab5ce47685e729a32ad6426f7d1b8273c
Abhi4710 <34485116+Abhi4710@users.noreply.github.com>
2019-07-16 20:07:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,58 +1,87 @@
 'use strict';
 
 const express = require('express');
-const bodyParser = require("body-parser");
+// const express1 = require('express');
 const PORT = process.env.PORT || 3000;
-const PORT_WS = 4000;
+const PORT_WH = 4000;
+const server = express().listen(PORT, () => console.log(`Listening on ${PORT}`));
 const app = express();
+const bodyParser = require("body-parser");
+const SocketServer = require('ws').Server;
+var resp = '';
+// const path = require('path');
+
+// const INDEX = path.join(__dirname, 'index.html');
 
 app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
+    bodyParser.urlencoded({
+        extended: true
+    })
 );
 
 app.use(bodyParser.json());
 
-app.post("/", function(req, res) {
-    console.log('echo');
-    console.log('Query:' + Object.entries(req.body.queryResult));
-    console.log('Param ' + Object.entries(req.body.queryResult.parameters));
-    var n = req.body.queryResult.queryText;
-    
-    if (n.includes("what")) {
-      var speech = 'Please wait checking device - ' + req.body.queryResult.parameters.device;}
-    else{
-     var speech =
-      req.body.queryResult &&
-      req.body.queryResult.parameters
-      ? "It is turned " + req.body.queryResult.parameters.state : "Seems like some problem. Speak again.";
-    }
-    
-    var req_d = Object.entries(req);
-    
-    var speechResponse = {
-      google: {
-        expectUserResponse: true,
-        richResponse: {
-          items: [
-            {
-              simpleResponse: {
-                textToSpeech: speech
-              }
-            }
-          ]
-        }
-      }
-    };
-    
-    return res.json({
-      payload: speechResponse,
-      //data: speechResponse,
-      fulfillmentText: "Sample text response",
-      speech: speech,
-      displayText: speech,
-      source: "webhook-echo-sample"
+
+const wss = new SocketServer({ server })
+
+wss.on('connection', function connection(ws) {
+    console.log('Client connected');
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message)
     });
-  });
-app.listen(PORT, () => console.log(`Webhook Listening on ${PORT}`))
+    ws.on('close', () => console.log('Client disconnected'));
+});
+
+function myfunction(resp) {
+    wss.clients.forEach((client) => {
+        client.send(JSON.stringify(resp));
+        // console.log(client.send('1234'));
+    });
+};
+
+app.post("/echo", function (req, res) {
+    console.log('echo');
+    console.log(req.body.queryResult);
+    resp = req.body.queryResult;
+    myfunction(resp);
+    // console.log('Query:' + Object.entries(req.body.queryResult.queryText));
+    // console.log('Param ' + Object.entries(req.body.queryResult.parameters));
+    var n = req.body.queryResult.queryText;
+
+    if (n.includes("what")) {
+        var speech = 'Please wait checking device - ' + req.body.queryResult.parameters.device;
+    }
+    else {
+        var speech =
+            req.body.queryResult &&
+                req.body.queryResult.parameters
+                ? "It is turned " + req.body.queryResult.parameters.state : "Seems like some problem. Speak again.";
+    }
+
+    var req_d = Object.entries(req);
+
+    var speechResponse = {
+        google: {
+            expectUserResponse: true,
+            richResponse: {
+                items: [
+                    {
+                        simpleResponse: {
+                            textToSpeech: speech
+                        }
+                    }
+                ]
+            }
+        }
+    };
+
+    return res.json({
+        payload: speechResponse,
+        //data: speechResponse,
+        fulfillmentText: "Sample text response",
+        speech: speech,
+        displayText: speech,
+        source: "webhook-echo-sample"
+    });
+});
+app.listen(PORT_WH, () => console.log(`Listening on ${PORT_WH}`))
