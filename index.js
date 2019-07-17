f22d68583d3eef83c063868f3e1fbe27ae755d85
Abhi4710 <34485116+Abhi4710@users.noreply.github.com>
2019-07-17 15:53:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 'use strict';
 
 const express = require('express');
-// const express1 = require('express');
-// const PORT_WS = process.env.PORT_WS || 3001;
-//const PORT = 3000;
 const PORT = process.env.PORT || 4001;
-// const server = express().use((req, res) => res.sendFile(INDEX)).listen(PORT_WS, () => console.log(`websocket Listening on ${PORT_WS}`));
 const server = express();
 const bodyParser = require("body-parser");
 const expressWs = require('express-ws')(server);
-// const SocketServer = require('ws').Server;
+
 var resp = '';
 var query = '';
-const path = require('path');
 
+const path = require('path');
 const INDEX = path.join(__dirname, 'index.html');
 
 server.use(
@@ -28,7 +24,7 @@ server.use(bodyParser.json());
 server.use(function (req, res, next) {
   console.log('middleware');
   req.testing = 'testing';
-    console.log('next function');
+  console.log('next function');
   return next();
 });
  
@@ -41,13 +37,19 @@ server.ws('/', function(ws, req) {
   ws.on('connect', () => console.log('client connected'));   
   ws.on('message', function(msg) {
     ws.send('ok ' + msg);
-    console.log(msg);
-      if (msg != '"heartbeat":"keepalive"') {
-        resp = msg;
-      }
+//     console.log(msg);
+//       if (msg != '"heartbeat":"keepalive"') {
+//         resp = msg;
+//       }
    });
-  setInterval(function(){ if (query != '') {ws.send(query);
-                                           query = '';}
+  setInterval(async function(){ if (query != '') {ws.send(query);
+                                            await ws.on('message', function(msg) {
+                                                console.log(msg);
+                                                if (msg != '"heartbeat":"keepalive"') {
+                                                    resp = msg;}
+                                            });
+                                            query = '';
+                                                 }
                         else {console.log('NO QUERY');}}, 3000);
   console.log('socket', req.testing);
 });
@@ -68,6 +70,7 @@ server.post("/echo", function (req, res) {
         var speech = 'Please wait checking device - ' + req.body.queryResult.parameters.device;
         if (resp != '') {
             var speech = resp;
+            resp = '';
         };
     }
     else {
