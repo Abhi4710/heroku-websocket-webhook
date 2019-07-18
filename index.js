1d5440b5f405a8d6d364becb02a258d3bd596dc9
Abhi4710 <34485116+Abhi4710@users.noreply.github.com>
2019-07-18 12:04:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ server.ws('/', function(ws, req) {
    });
     
    setInterval(function(){if (g_query == '?') {ws.send("?"); g_query = null;}
-                         else if (g_query == 'CMD:on' || g_query == 'CMD:off')) {ws.send(g_query); g_query = null;}}, 1000);
+                         else if (g_query == 'CMD:on' || g_query == 'CMD:off') {ws.send(g_query); g_query = null;}}, 1000);
 });
 
 function myfunction(query, resp) {
